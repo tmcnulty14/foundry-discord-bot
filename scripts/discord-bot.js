b1735b3fd9c4076d79d9efb5a6a1e8aa11069dd0
Thomas McNulty <tommcn.14@gmail.com>
2024-07-10 12:24:54 -0400

    Log Hello World

---
+++ scripts/discord-bot.js
@@ -0,0 +1,9 @@
+console.log("[Discord Bot] Loaded.");
+
+Hooks.on("init", function() {
+  console.log("[Discord Bot] Initializing.");
+});
+
+Hooks.on("ready", function() {
+  console.log("[Discord Bot] Ready. Hello World!");
+});

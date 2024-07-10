d9b530976ff95c90ab1a1d176143e882d1d8305c
Thomas McNulty <tommcn.14@gmail.com>
2024-07-10 14:25:18 -0400

    Login to discord bot
    Add discord bot token setting to allow login.
    Also limit bot login to GM only.

--- scripts/discord-bot.js
+++ scripts/discord-bot.js
@@ -1,16 +1,50 @@
+// Require the necessary discord.js classes
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+
 class DiscordBot {
+    static CLIENT = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+    static CLIENT_READY = false
+
     static initialize() {
         Logging.debug("Initializing.");
 
-        // Nothing to do right now.
-        // Might be used for registering settings later.
+        this.createSettings();
+
+        if (game.user.role !== 4) { // USER_ROLES.GAMEMASTER = 4
+            Logging.log("Not logged in as GM. Discord bot only runs when logged in as the GM.");
+            return;
+        }
+
+        const token = game.settings.get('discord-bot', 'discordToken');
+        if (!token) {
+            Logging.log('No discord token configured, skipping bot login.');
+            return;
+        }
+
+        // Login the discord bot client.
+        CLIENT.once(Events.ClientReady, readyClient => {
+            Logging.log(`Discord client ready! Logged in as ${readyClient.user.tag}`);
+            CLIENT_READY = true
+        });
+        CLIENT.login(token);
+    }
+
+    static createSettings() {
+        game.settings.register('discord-bot', 'discordToken', {
+            name: 'Discord Token',
+            hint: 'The discord bot token to use for authenticating as the bot user.',
+            scope: 'client',
+            config: true,
+            type: String,
+            default: "",
+            onChange: value => { },
+            requiresReload: true,
+        })
     }
 
     static onReady() {
-        Logging.debug("Ready. Hello World!");
-
-        // Nothing to do right now.
-        // Might be used to connect the discord bot later.
+        Logging.debug("Ready.");
     }
 }
 
@@ -20,14 +54,14 @@ class Logging {
     /**
      * Log helper.
      **/
-    static log(...args) {  
+    static log(...args) {
         console.log(`[${this.ID}]`, ...args);
     }
 
     /**
      * Log helper.
      **/
-    static debug(...args) {  
+    static debug(...args) {
         console.debug(`[${this.ID}]`, ...args)
     }
 

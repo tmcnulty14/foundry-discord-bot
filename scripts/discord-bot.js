2399fac9028417b7633bbe4f91b1a91cec596054
Thomas McNulty <tommcn.14@gmail.com>
2024-07-10 12:32:28 -0400

    Set up DiscordBot class

--- scripts/discord-bot.js
+++ scripts/discord-bot.js
@@ -1,9 +1,60 @@
-console.log("[Discord Bot] Loaded.");
+Logging.debug("Loaded.");
 
 Hooks.on("init", function() {
-  console.log("[Discord Bot] Initializing.");
+    DiscordBot.initialize()
 });
 
 Hooks.on("ready", function() {
-  console.log("[Discord Bot] Ready. Hello World!");
 });
+
+class DiscordBot {
+    static initialize() {
+        Logging.debug("Initializing.");
+
+        // Nothing to do right now.
+        // Might be used for registering settings later.
+    }
+
+    static onReady() {
+        Logging.debug("Ready. Hello World!");
+
+        // Nothing to do right now.
+        // Might be used to connect the discord bot later.
+    }
+}
+
+class Logging {
+    static ID = 'discord-bot'
+
+    /**
+     * Log helper.
+     **/
+    static log(...args) {  
+        console.log(this.ID, '|', ...args);
+    }
+
+    /**
+     * Log helper.
+     **/
+    static debug(...args) {  
+        const shouldLog = game.modules.get('_dev-mode')?.api?.getPackageDebugValue(this.ID);
+
+        if (shouldLog) {
+            console.log(this.ID, '|', ...args);
+        }
+    }
+
+    /**
+     * Warning log helper.
+     **/
+    static warn(...args) {
+        console.warn(this.ID, '|', ...args);
+    }
+
+    /**
+     * Error log helper.
+     **/
+    static error(...args) {
+        console.error(this.ID, '|', ...args);
+    }
+}
